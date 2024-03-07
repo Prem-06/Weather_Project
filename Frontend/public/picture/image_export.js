20090d332811976ec77bae5f5cd4b28d0f54aba3
Prem Prakash Pal <0610prem@gmail.com>
2024-03-07 15:28:53 +0530

    day1 second commit

---
+++ Frontend/public/picture/image_export.js
@@ -0,0 +1,7 @@
+const cloudy='/cloudy.png';
+const sunny='/sunny.png';
+const partly_cloudy='/partly_cloudy.png';
+const container={
+    cloudy,sunny,partly_cloudy
+}
+export default container;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-
+import Signup from './components/signup/signup.jsx'
+import Login from './components/login/login.jsx'
+import Home from './components/home/home.jsx'
 function App() {
   
 
   return (
     
      <div>
-      hello
+        <Home/>
      </div>
     
   )
